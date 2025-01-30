34dbb60433487d5bdf00a974fb3fc1ade89a87d2
Ellucidator <andradefj13@hotmail.com>
2025-01-30 13:21:49 -0300

    feat: Controller do upload

---
+++ src/main/java/com/handler/file/controllers/FileController.java
@@ -0,0 +1,29 @@
+package com.handler.file.controllers;
+
+
+import com.handler.file.services.FileStorageService;
+import com.handler.file.vo.v1.UploadFileResponseVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @PostMapping("/uploadFile")
+    public UploadFileResponseVO uploadFile(@RequestParam("file")MultipartFile file){
+        String fileName = fileStorageService.storeFile(file);
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/files/")
+                .path(fileName)
+                .toUriString();
+        return new UploadFileResponseVO(fileName, fileDownloadUri,file.getContentType(), file.getSize());
+    }
+}
