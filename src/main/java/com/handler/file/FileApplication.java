090c1a855ba32a0c3cffb8d48195e9bc38edf2b5
Ellucidator <andradefj13@hotmail.com>
2025-01-30 14:38:33 -0300

    feat: Configurações adicionais

--- src/main/java/com/handler/file/FileApplication.java
+++ src/main/java/com/handler/file/FileApplication.java
@@ -1,9 +1,18 @@
 package com.handler.file;
 
+import com.handler.file.config.FileStorageConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableAutoConfiguration
+@EnableConfigurationProperties({
+		FileStorageConfig.class
+})
 @SpringBootApplication
+@ComponentScan
 public class FileApplication {
 
 	public static void main(String[] args) {
