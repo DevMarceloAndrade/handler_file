3651ad4356e4b9a078611feb4be78f73b8ad5ea1
Ellucidator <andradefj13@hotmail.com>
2025-01-30 13:20:13 -0300

    feat: exceptions e service

---
+++ src/main/java/com/handler/file/services/FileStorageService.java
@@ -0,0 +1,46 @@
+package com.handler.file.services;
+
+
+import com.handler.file.config.FileStorageConfig;
+import com.handler.file.exceptions.FileStorageException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(FileStorageConfig fileStorageConfig) {
+        this.fileStorageLocation = Paths.get(fileStorageConfig.getUploadDir())
+                .toAbsolutePath()
+                .normalize();
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception e) {
+            throw new FileStorageException("Could not create the directory where the uploaded files",e);
+        }
+    }
+
+    public String storeFile(MultipartFile file){
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+            if (fileName.contains("..")){
+                throw new FileStorageException("Invalid name");
+            }
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return fileName;
+        } catch (Exception e) {
+            throw new FileStorageException("Could not store file" + fileName, e);
+        }
+    }
+}
